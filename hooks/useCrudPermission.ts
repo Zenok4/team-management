aed61c434dbedcb3a053fbf9cc2d6e476a240030
Zenok4 <dungk4.camlo@gmail.com>
2026-03-26 19:41:53 +0700

    delete files unecessary

--- hooks/useCrudPermission.ts
+++
@@ -1,9 +0,0 @@
-// hooks/useCrudPermission.ts
-import { PermissionAction } from "@/types/permission-type";
-import useCan from "./useCan";
-
-const useCrudPermission = (resource: string, action: PermissionAction) => {
-  return useCan(`${resource}:${action}`);
-};
-
-export default useCrudPermission;
